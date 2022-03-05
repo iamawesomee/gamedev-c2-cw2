f33aa4ea534a4581c60369b437ba12c47611469f
fatema-mohammad-alenezi-472 <100342867+iamawesomee@users.noreply.github.com>
2022-03-05 13:22:26 +0300

    done

---
+++ cw6/cw6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string heroName = "Thor";
+            string heroSuperpower1 = " I can fly using a hammer"; 
+            string heroSuperpower2 = "I can summon lightning";
+            int heroHeight = 213
+            int herAge = 1480
+
+        }
+    }
+}
